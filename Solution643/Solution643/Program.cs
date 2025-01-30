2a09679e7a769ee35030f2fe129868f2d2deec2b
saparov-codes <saparov.inbox@gmail.com>
2025-01-30 15:50:00 +0500

    some solutions added!

---
+++ Solution643/Solution643/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Solution643;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Size Array : ");
+        var size = int.Parse(Console.ReadLine());
+        var array = GetArray(size);
+        Console.Write("count of group elements : ");
+        var group = int.Parse(Console.ReadLine());
+
+        Solution solution = new Solution();
+
+        var maxAvarage = solution.FindMaxAverage(array, group);
+        Console.Write("Maximum Avarage is : " + maxAvarage);
+
+    }
+
+    public static int[] GetArray(int size)
+    {
+        var array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"{i+1}-Number : ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        return array;
+    }
+}
---
+++ Solution643/Solution643/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace Solution643;
+
+public class Solution
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        if (k <= 0 || k > nums.Length)
+        {
+            throw new ArgumentException("group elements must be positive and less than Array size!");
+        }
+
+        var sum = 0d;
+        for (int i = 0;i < k; i++)
+        {
+            sum += nums[i];
+        }
+        var max = sum;
+
+        for (int i = k; i < nums.Length;i++)
+        {
+            sum = sum - nums[i - k] + nums[i];
+            max = Math.Max(max, sum);
+        }
+        
+
+        return max / k;
+    }
+}
